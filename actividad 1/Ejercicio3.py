f73209b7db5c0b03c002663a4c61f74e81542c14
Maximo Blazquez Tomas <maxi05blazquez@gmail.com>
2022-04-22 08:50:20 -0300

    ejercicio 3

--- actividad 1/Ejercicio3.py
+++ actividad 1/Ejercicio3.py
@@ -1,2 +1,13 @@
-for i in range(1,6):
-    print(i*'*')
+print("ejercicio N3")
+
+def crearcuadrado():
+ for i in range (ancho):
+    for k in range (largo):
+        print(objeto," ",fin="")
+    print("")
+
+
+ancho = int(input("escirbe ancho"))
+largo = int(input("escribe el largo"))
+objeto = input("caracter a poner")
+crearcuadrado()
