771c6bd5c89187e9cf268d4dbd697ce3fbe5e368
Maximo Blazquez Tomas <maxi05blazquez@gmail.com>
2022-05-06 08:29:26 -0300

    Clase 3 terminada

--- actividad_2/Clase3.py
+++ actividad_2/Clase3.py
@@ -10,12 +10,23 @@ class Triangulo():
         self.equilatero()
         self.lado_mayor()
 
-    def lado_mayor():
-        S
-
-
+    def lado_mayor(self):
+        if (self.lado1 > self.lado2):
+            self.mayor = self.lado1
+            if (self.mayor > self.lado3):
+                 print("El lado mayor es el lado 1 que mide:", self.mayor)
+            else:
+                 self.mayor = self.lado3
+                 print("el lado mayot es el lado 3 que mide:", self.mayor)
+        else:
+            self.mayor = self.lado2
+            if (self.mayor > self.lado3):
+                 print("El lado mayor es el lado 2 que mide:", self.mayor)
+            else:
+                 self.mayor = self.lado3
+                 print("el lado mayor es el lado 3 que mide:", self.mayor)
     def equilatero(self):
-        if self.lado1 == self.lado2 == self.lado3:
+        if (self.lado1 == self.lado2  and self.lado2 == self.lado3):
             print("es un triangulo equilatero")
         else:
             print("no es un triangulo equilatero")
